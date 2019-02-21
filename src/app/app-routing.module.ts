fcb8ff005861c6ece317764198a13eeff1ccba61
Sahil <er.sahilvig@gmail.com>
2019-02-21 17:52:41 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: UsersComponent
+    },
+    {
+        path: 'posts',
+        component: PostsComponent
+    },
+    {
+        path: 'details/:id',
+        component: DetailsComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
